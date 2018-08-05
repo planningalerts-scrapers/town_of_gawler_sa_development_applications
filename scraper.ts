6a7f8e8cccae8c6ac7e9f9e673d26e10c76763eb
MichaelBone <Michael.Bone@senet.com.au>
2018-08-05 17:36:39 +0930

    Initial implementation.

--- scraper.ts
+++ scraper.ts
@@ -7,17 +7,22 @@
 //
 // Michael Bone
 // 5th August 2018
+
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cheerio = require("cheerio");
-const request = require("request-promise-native");
-const sqlite3 = require("sqlite3");
-const moment = require("moment");
+
+import * as cheerio from "cheerio";
+import * as request from "request-promise-native";
+import * as sqlite3 from "sqlite3";
+import * as moment from "moment";
+
 sqlite3.verbose();
+
 const DevelopmentApplicationMainUrl = "https://eservices.gawler.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=4&nodeNum=3228";
 const DevelopmentApplicationSearchUrl = "https://eservices.gawler.sa.gov.au/eservice/daEnquiry.do?number=&lodgeRangeType=on&dateFrom={0}&dateTo={1}&detDateFromString=&detDateToString=&streetName=&suburb=0&unitNum=&houseNum=0%0D%0A%09%09%09%09%09&planNumber=&strataPlan=&lotNumber=&propertyName=&searchMode=A&submitButton=Search";
 const CommentUrl = "mailto:council@gawler.sa.gov.au";
+
 // Sets up an sqlite database.
+
 async function initializeDatabase() {
     return new Promise((resolve, reject) => {
         let database = new sqlite3.Database("data.sqlite");
@@ -27,7 +32,9 @@ async function initializeDatabase() {
         });
     });
 }
+
 // Inserts a row in the database if it does not already exist.
+
 async function insertRow(database, developmentApplication) {
     return new Promise((resolve, reject) => {
         let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
@@ -41,56 +48,67 @@ async function insertRow(database, developmentApplication) {
             developmentApplication.receivedDate,
             null,
             null
-        ], function (error, row) {
+        ], function(error, row) {
             if (error) {
                 console.error(error);
                 reject(error);
-            }
-            else {
+            } else {
                 if (this.changes > 0)
                     console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" into the database.`);
                 else
                     console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" because it was already present in the database.`);
-                sqlStatement.finalize(); // releases any locks
+                sqlStatement.finalize();  // releases any locks
                 resolve(row);
             }
         });
     });
 }
+
 // Parses the development applications.
+
 async function main() {
     // Ensure that the database exists.
+
     let database = await initializeDatabase();
+
     // Retrieve the main page.
+
     console.log(`Retrieving page: ${DevelopmentApplicationMainUrl}`);
-    let jar = request.jar(); // this cookie jar will end up containing the JSESSIONID_live cookie after the first request; the cookie is required for the second request
+    let jar = request.jar();  // this cookie jar will end up containing the JSESSIONID_live cookie after the first request; the cookie is required for the second request
     await request({ url: DevelopmentApplicationMainUrl, jar: jar, rejectUnauthorized: false });
+
     // Retrieve the results of a search for the last month.
+
     let dateFrom = encodeURIComponent(moment().subtract(1, "months").format("DD/MM/YYYY"));
     let dateTo = encodeURIComponent(moment().format("DD/MM/YYYY"));
     let developmentApplicationSearchUrl = DevelopmentApplicationSearchUrl.replace(/\{0\}/g, dateFrom).replace(/\{1\}/g, dateTo);
     console.log(`Retrieving search results for: ${developmentApplicationSearchUrl}`);
-    let body = await request({ url: developmentApplicationSearchUrl, jar: jar, rejectUnauthorized: false }); // the cookie jar contains the JSESSIONID_live cookie
+    let body = await request({ url: developmentApplicationSearchUrl, jar: jar, rejectUnauthorized: false });  // the cookie jar contains the JSESSIONID_live cookie
     let $ = cheerio.load(body);
+
     // Parse the search results.
+
     for (let headerElement of $("h4.non_table_headers").get()) {
-        let address = $(headerElement).text().trim().replace(/\s\s+/g, " "); // reduce multiple consecutive spaces in the address to a single space
+        let address: string = $(headerElement).text().trim().replace(/\s\s+/g, " ");  // reduce multiple consecutive spaces in the address to a single space
         let applicationNumber = "";
         let reason = "";
         let receivedDate = moment.invalid();
+
         for (let divElement of $(headerElement).next("div").get()) {
             for (let paragraphElement of $(divElement).find("p.rowDataOnly").get()) {
-                let key = $(paragraphElement).children("span.key").text().trim();
-                let value = $(paragraphElement).children("span.inputField").text().trim();
+                let key: string = $(paragraphElement).children("span.key").text().trim();
+                let value: string = $(paragraphElement).children("span.inputField").text().trim();
                 if (key === "Type of Work")
                     reason = value;
                 else if (key === "Application No.")
                     applicationNumber = value;
                 else if (key === "Date Lodged")
-                    receivedDate = moment(value, "D/MM/YYYY", true); // allows the leading zero of the day to be omitted
+                    receivedDate = moment(value, "D/MM/YYYY", true);  // allows the leading zero of the day to be omitted
             }
         }
+
         // Ensure that at least an application number and address have been obtained.
+
         if (applicationNumber !== "" && address !== "") {
             await insertRow(database, {
                 applicationNumber: applicationNumber,
@@ -104,5 +122,5 @@ async function main() {
         }
     }
 }
+
 main().then(() => console.log("Complete.")).catch(error => console.error(error));
-//# sourceMappingURL=scraper.js.map
