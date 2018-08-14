b88ae0681112a40bd6b797b4de1fe6e7c1dba637
MichaelBone <Michael.Bone@senet.com.au>
2018-08-14 20:10:37 +0930

    Changed to automatically compile TypeScript when launching the debugger in VSCode.

--- scraper.ts
+++ scraper.ts
@@ -1,10 +1,6 @@
 // Parses the development applications at the South Australian Town of Gawler web site and places
 // them in a database.
 //
-// In each VSCode session: to automatically compile this TypeScript script into JavaScript whenever
-// the TypeScript is changed and saved, press Ctrl+Shift+B and select "tsc:watch - tsconfig.json".
-// This starts a task that watches for changes to the TypeScript script.
-//
 // Michael Bone
 // 5th August 2018
 
