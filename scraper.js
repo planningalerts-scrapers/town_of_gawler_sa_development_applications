d92dcc50dda83f2cde6e2f275ebb770b79de7d8c
Matthew Landauer <mlandauer@gmail.com>
2019-05-08 11:27:58 +1000

    Always save record as planningalerts now does versioning

--- scraper.js
+++ scraper.js
@@ -30,7 +30,7 @@ async function initializeDatabase() {
 // Inserts a row in the database if it does not already exist.
 async function insertRow(database, developmentApplication) {
     return new Promise((resolve, reject) => {
-        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        let sqlStatement = database.prepare("insert or replace into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
         sqlStatement.run([
             developmentApplication.applicationNumber,
             developmentApplication.address,
@@ -47,10 +47,7 @@ async function insertRow(database, developmentApplication) {
                 reject(error);
             }
             else {
-                if (this.changes > 0)
-                    console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" into the database.`);
-                else
-                    console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" because it was already present in the database.`);
+                console.log(`    Saved: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" into the database.`);
                 sqlStatement.finalize(); // releases any locks
                 resolve(row);
             }
